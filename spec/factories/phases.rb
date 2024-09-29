17bed1d54533d23e4fcf4a47e93669c3ea75d496
“Mário <mariosergioadriano@gmail.com>
2024-09-29 23:58:27 +0000

    Executando annotate

--- spec/factories/phases.rb
+++ spec/factories/phases.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: phases
+#
+#  id             :bigint           not null, primary key
+#  deleted_at     :datetime
+#  description    :text
+#  is_end         :boolean
+#  name           :string
+#  position       :integer
+#  projects_count :integer          default(0), not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+# Indexes
+#
+#  index_phases_on_deleted_at  (deleted_at)
+#  index_phases_on_position    (position)
+#
 FactoryBot.define do
   factory :phase do
     name { "Phase #{rand(1000)}" }
--- spec/models/administrator_spec.rb
+++ spec/models/administrator_spec.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  deleted_at             :datetime
+#  email                  :string           default("")
+#  encrypted_password     :string           default(""), not null
+#  is_reset_password      :boolean          default(FALSE)
+#  name                   :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  type                   :string
+#  username               :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_deleted_at            (deleted_at)
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_username              (username) UNIQUE
+#
 require 'rails_helper'
 
 RSpec.describe Administrator, type: :model do
